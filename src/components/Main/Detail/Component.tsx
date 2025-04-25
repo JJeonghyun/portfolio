2b65d2d862f031403d98a1be47547e46c48921bb
JJeongHyun <ghkdwja9649@gmail.com>
2025-04-25 11:18:14 +0900

    chore: 내정보 컴포넌트 정리

--- src/components/Main/Detail/Component.tsx
+++ src/components/Main/Detail/Component.tsx
@@ -1,6 +1,7 @@
+import Image from 'next/image';
 import styled from 'styled-components';
 
-import ItemComponent from './Item/Component';
+import {INFO} from './contant';
 
 const DetailComponent = ({
   aboutRef,
@@ -11,7 +12,19 @@ const DetailComponent = ({
     <AboutMeContainer>
       <div ref={aboutRef}>ABOUT ME</div>
       <div>
-        <ItemComponent />
+        <div>
+          {INFO.map((item, index) => (
+            <div key={`item-outer-${index}`}>
+              <div key={`first-item-${index}`}>
+                <Image src={item.img} alt="" key={`item-img-${index}`} />
+              </div>
+              <div key={`second-item-outer-${index}`}>
+                <div key={`first-inner-item-${index}`}>{item.name}</div>
+                <div key={`second-inner-item-${index}`}>{item.text}</div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
       <div>연락은 편하게 언제든지 메일, DM 및 TISTORY로 부탁드립니다.</div>
     </AboutMeContainer>
@@ -35,6 +48,42 @@ const AboutMeContainer = styled.div`
     width: 90%;
     margin: 0 auto;
     padding: 15px 0;
+    & > div {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      flex-wrap: wrap;
+      @media only screen and (max-width: 426px) {
+        justify-content: center;
+      }
+      & > div {
+        width: 30%;
+        display: flex;
+        padding: 15px 0;
+        @media only screen and (max-width: 768px) {
+          width: 50%;
+        }
+        @media only screen and (max-width: 426px) {
+          width: 80%;
+        }
+        & > div {
+          width: 49%;
+          margin: 0 auto;
+          &:first-child {
+            width: 50px;
+            margin: 0;
+            & > img {
+              width: 100%;
+              height: auto;
+            }
+          }
+          & > div {
+            width: 100%;
+          }
+        }
+      }
+    }
   }
   & > div:last-child {
     width: 100%;
